77f7f7c9be049ecdc68900ed65698f1c37321be6
Khagendra Khatri <52613596+Khagendra01@users.noreply.github.com>
2023-05-25 11:38:59 -0400

    minimum distance with BF and DP

--- EditDistanceBF.cpp
+++ EditDistanceBF.cpp
@@ -25,7 +25,50 @@ int minED(string f, string s, int fL, int sL)
     return min(ins, min(del, rep));
 }
 
+int recMinEDDP(string f, string s, int fL, int sL)
+{
+    if (dpMem[fL][sL] != -1)
+    {
+        return dpMem[fL][sL];
+    }
 
+    if (fL == 0)
+    {
+        dpMem[fL][sL] = sL;
+    }
+    else if (sL == 0)
+    {
+        dpMem[fL][sL] = fL;
+    }
+    else
+    {
+
+        int inst = 1 + recMinEDDP(f, s, fL - 1, sL);
+        int del = 1 + recMinEDDP(f, s, fL, sL - 1);
+        int mismat = recMinEDDP(f, s, fL - 1, sL - 1) + ((f[fL] == s[sL]) ? 0 : 1);
+
+        dpMem[fL][sL] = min(inst, min(del, mismat));
+    }
+    return dpMem[fL][sL];
+}
+
+int recMinEDDyp(string f, string s, int fL, int sL)
+{
+    dpMem = new int *[fL + 1];
+    for (int i = 0; i <= fL; i++)
+    {
+        dpMem[i] = new int[sL + 1];
+    }
+
+    for (int i = 0; i <= fL; i++)
+    {
+        for (int j = 0; j <= sL; j++)
+        {
+            dpMem[i][j] = -1;
+        }
+    }
+    return recMinEDDP(f, s, fL, sL);
+}
 
 int main()
 {
@@ -34,6 +77,6 @@ int main()
     string second = "BDDLE";
 
     cout << minED(first, second, first.size(), second.size()) << "\n";
-    //cout << recMinEDDyp(first, second, first.size(), second.size());
+    cout << recMinEDDyp(first, second, first.size(), second.size());
     return 0;
 }
