c3f4993e3dd3b21e8044e2eaa0cb1dd6540becb0
Khagendra Khatri <52613596+Khagendra01@users.noreply.github.com>
2023-05-26 11:19:57 -0400

    heap sort algorithm

--- Heapsort.cpp
+++ Heapsort.cpp
@@ -11,30 +11,37 @@ void heapify(int *arr, int n, int p)
 {
     int largest = p;
     int l = 2 * p + 1;
-    int r = 2* p + 2;
+    int r = 2 * p + 2;
 
-    if(l < n && arr[l] > arr[largest])
+    if (l < n && arr[l] > arr[largest])
     {
         largest = l;
     }
-     if(r < n && arr[l] > arr[largest])
+    if (r < n && arr[r] > arr[largest])
     {
         largest = r;
     }
 
-    if( largest!= p ){
-        
+    if (largest != p)
+    {
+        swap(arr[largest], arr[p]);
+
+        heapify(arr, n, largest);
     }
-
 }
-
-  
 
 void heapSort(int *arr, int n)
 {
-   for(int i = n/2 -1; i >=0; i ++ )
-    buildTheHeap(arr, n, 1);
+    for (int i = n / 2 - 1; i >= 0; i--)
+    {
+        heapify(arr, n, i);
+    }
 
+    for (int i = n - 1; i > 0; i--)
+    {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
 
     return;
 }
