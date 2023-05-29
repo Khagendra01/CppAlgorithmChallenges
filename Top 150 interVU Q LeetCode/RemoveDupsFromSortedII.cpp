7aeae9a4ce319202ffbe661e5029e64e479a63cf
Khagendra Khatri <52613596+Khagendra01@users.noreply.github.com>
2023-05-29 13:09:00 -0400

    Add files via upload

---
+++ Top 150 interVU Q LeetCode/RemoveDupsFromSortedII.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int removeElementII(vector<int> nums)
+{
+    if( nums.size() <= 2 )
+    {
+        return nums.size();
+    }
+    int k = 2;
+    
+    for( int i = 2; i < nums.size(); i++)
+    {
+        if(nums[i] != nums[k-2])
+        {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
+}
+
+int main()
+{
+     vector<int> nums = {0,0,1,1,1,1,2,3,3};
+        cout << removeElementII( nums);
+}
