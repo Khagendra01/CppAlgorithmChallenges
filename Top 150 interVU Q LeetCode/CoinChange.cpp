949c60d850377efefb19ea556aee7227f10de8d6
Khagendra Khatri <52613596+Khagendra01@users.noreply.github.com>
2023-06-02 11:41:50 -0400

    works but time limit exceed on leetcode

--- Top 150 interVU Q LeetCode/CoinChange.cpp
+++ Top 150 interVU Q LeetCode/CoinChange.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// this is not correct method
 int coinRecu(vector<int> &coins, int amount, int i)
 {
     if (amount == 0)
@@ -8,7 +9,7 @@ int coinRecu(vector<int> &coins, int amount, int i)
         return 0;
     }
 
-    if( i < 0 || amount < 0)
+    if (i < 0 || amount < 0)
     {
         return -1;
     }
@@ -22,34 +23,65 @@ int coinRecu(vector<int> &coins, int amount, int i)
         int include = coinRecu(coins, amount - coins[i], i);
         int noInclude = coinRecu(coins, amount, i - 1);
 
-        if(include == -1 && noInclude == -1)
+        if (include == -1 && noInclude == -1)
         {
             return -1;
         }
 
-        if(include == -1)
+        if (include == -1)
         {
             return noInclude + 1;
         }
 
-          if(noInclude == -1)
+        if (noInclude == -1)
         {
             return include + 1;
         }
-
 
         return min(include, noInclude) + 1;
     }
 }
 
+int coinDP(vector<int> &coins, int amount, vector<int> &DP)
+{
+    if (amount == 0)
+    {
+        return 0;
+    }
+    if (amount < 0)
+    {
+        return -1;
+    }
+    if (DP[amount] != -1)
+    {
+        return DP[amount];
+    }
+
+    int n = INT_MAX;
+    for (int coin : coins)
+    {
+        if (amount >= coin)
+        {
+            int val = coinDP(coins, amount - coin, DP);
+            if (val != -1)
+            {
+                n = min(val + 1, n);
+            }
+        }
+    }
+    DP[amount] = (n == INT_MAX) ? -1 : n;
+    return DP[amount];
+}
+
 int coinChange(vector<int> &coins, int amount)
 {
-    return coinRecu(coins, amount, coins.size() - 1);
+    vector<int> DP(amount + 1, -1);
+    return coinDP(coins, amount, DP);
 }
 
 int main()
 {
-    vector<int> coins = {1,2,5};
+    vector<int> coins = {1, 2, 5};
     cout << coinChange(coins, 11);
-    return 0; 
+    return 0;
 }
