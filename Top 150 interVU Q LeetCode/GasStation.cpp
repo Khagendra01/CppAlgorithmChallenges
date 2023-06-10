262faff38db8e2336681b393b463b19cff3f1e92
Khagendra Khatri <52613596+Khagendra01@users.noreply.github.com>
2023-06-10 11:08:13 -0400

    half done

---
+++ Top 150 interVU Q LeetCode/GasStation.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int nxt(int i, vector<int>& gas, )
+    {
+        return (gas.size() - 1 == i) ? 0: i+1;
+    }
+
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+       int index = 0;
+       int tank= 0;
+       while()
+       {
+           int i = index;
+           while()
+           {
+           int tank = gas[i] - cost[i];
+           i = nxt(i, gas);
+           }
+       }
+    }
+
+    int main()
+{
+
+    return 0;
+}
