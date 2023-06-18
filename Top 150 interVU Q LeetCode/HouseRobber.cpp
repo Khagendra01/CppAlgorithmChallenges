c15bed4489691283847f0dcc6d9e8cc38524712b
Khagendra Khatri <52613596+Khagendra01@users.noreply.github.com>
2023-06-18 12:06:34 -0400

    beats 100% in 0ms

--- Top 150 interVU Q LeetCode/HouseRobber.cpp
+++ Top 150 interVU Q LeetCode/HouseRobber.cpp
@@ -1,18 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-  int robBF(vector<int>& nums, int n)
+  int robBF(vector<int>& nums, int n, vector<int>& cache)
     {
         if( n <= -1)
         {
             return 0;
         }
 
-        return max(robBF(nums, n - 2) + nums[n], robBF(nums, n-1));
+        if(cache[n] != -1)
+        {
+            return cache[n];
+        }
+
+        cache[n] = max(robBF(nums, n - 2, cache) + nums[n], robBF(nums, n-1, cache));
+
+        return cache[n];
     }
 
     int rob(vector<int>& nums) {
-        return robBF(nums, nums.size() - 1);
+        vector<int> cache(nums.size(), -1);
+        return robBF(nums, nums.size() - 1, cache);
     }
 
     int main()
