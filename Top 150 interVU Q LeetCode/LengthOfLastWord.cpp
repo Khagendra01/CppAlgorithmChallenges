12b5a740d0d8e62e62809cbf3fb038c81004ed0e
Khagendra Khatri <52613596+Khagendra01@users.noreply.github.com>
2023-06-20 00:41:53 -0400

    sweet solution

--- Top 150 interVU Q LeetCode/LengthOfLastWord.cpp
+++ Top 150 interVU Q LeetCode/LengthOfLastWord.cpp
@@ -1,8 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int lengthOfLastWord(string s)
+{
+    int l = 0;
+    int last;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] != ' ')
+        {
+            l++;
+        }
+        else
+        {
+            if (l != 0)
+            {
+                last = l;
+            }
+            l = 0;
+        }
+    }
+    return (l != 0) ? l : last;
+}
+
 int main()
 {
-     cout << "Length of last word";
+    cout << lengthOfLastWord("Length of last word");
     return 0;
 }
